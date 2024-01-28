13969004257cf0881128502f446060fe4fd23f47
namnam7171 <55042341+namnam7171@users.noreply.github.com>
2024-01-28 15:28:09 +0900

    Revert "Feature/get posting detail"

--- src/main/java/com/erica/gamsung/posting/repository/PostingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.erica.gamsung.posting.repository;
-
-import com.erica.gamsung.posting.domain.Posting;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostingRepository extends JpaRepository<Posting, Long> {
-}
