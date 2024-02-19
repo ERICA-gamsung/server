2c17aab62dc0426052fc1a5ad746f950c841b274
오유정 <ohyujeong@oyujeong-ui-MacBookPro.local>
2024-02-19 15:53:41 +0900

    cors 임시 설정

---
+++ src/main/java/com/erica/gamsung/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.erica.gamsung.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") //보안 문제로 후에 수정 필요
+                .allowedMethods("GET", "PUT");
+    }
+}
