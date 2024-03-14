4deed5c259c53982665b4c6487681b564dbcee62
오유정 <ohyujeong@oyujeong-ui-MacBookPro.local>
2024-03-14 19:04:08 +0900

    swagger 추가

--- src/main/java/com/erica/gamsung/image/controller/ImageController.java
+++ src/main/java/com/erica/gamsung/image/controller/ImageController.java
@@ -2,21 +2,18 @@ package com.erica.gamsung.image.controller;
 import com.erica.gamsung.image.service.ImageService;
 import com.erica.gamsung.image.service.PostImageRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Objects;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/image")
 public class ImageController {
     final private ImageService imageService;
-    @PostMapping(path = "post/{postingId}")
-    public List<String> postImage(PostImageRequest postImageRequest, @PathVariable Long postingId) {
+    @PostMapping(path = "post/{postingId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<String> postImage(@ModelAttribute PostImageRequest postImageRequest, @PathVariable Long postingId) {
         return imageService.uploadImage(postImageRequest,postingId);
     }
 }
