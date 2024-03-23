8c69abe123cf33bc044853ae34f8a8ee5f46ce26
namnam99 <onep5286@gmail.com>
2024-03-23 16:03:44 +0900

    refactor : 더미데이터 수정

--- src/main/java/com/erica/gamsung/posting/service/PostingService.java
+++ src/main/java/com/erica/gamsung/posting/service/PostingService.java
@@ -6,7 +6,9 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 //@Component
@@ -35,6 +37,20 @@ public class PostingService {
         return null;
     }
 
+    public void selectOption(SelectOptionRequest request) {
+        PostingOption postingOption = new PostingOption(
+                request.getReservationId(),
+                request.getReservedAt(),
+                request.getMenu(),
+                request.getEvent(),
+                request.getComent()
+        );
+
+        postingRepository.save(postingOption);
+
+        return request;
+    }
+
     @PostConstruct
     public void init() {
         // 데이터가 이미 존재하면 바로 리턴
@@ -45,38 +61,49 @@ public class PostingService {
         // 더미 데이터 작성
         Posting posting1 = new Posting(
                 1L,
-                -1L,
-                -1L,
-                "http://example.s3.com/image1.png",
+                5L,
+                LocalDate.of(2024, 1, 24),
+                LocalTime.of(17, 0),
+                "김치찌개",
                 null,
-                "ready",
-                LocalDateTime.of(2024, 1, 24, 17, 0, 0),
-                List.of("우리 가게로 놀러오세요!", "오늘 요리 맛있습니다!", "너만 오면 고!")
+                null,
+                List.of("우리 가게로 놀러오세요!", "오늘 요리 맛있습니다!", "너만 오면 고!"),
+                "안녕하세요! 오늘은 김치찌개가 준비되어 있습니다. 많이 오세요!",
+                "http://example.s3.com/image1.png",
+                "ready"
         );
 
         Posting posting2 = new Posting(
                 2L,
-                -1L,
-                -1L,
+                61L,
+                LocalDate.of(2024, 3, 4),
+                LocalTime.of(9, 0),
+                "돈코츠 라멘",
+                "4명당 음료수 1개 무료",
+                "새학기 힘내세요!",
+                List.of("오늘은 1000원 할인", "라멘 드세요", "너만 오면 고!"),
+                "안녕하세요! 오늘은 돈코츠 라멘이 준비되어 있습니다. 많이 오세요!",
                 "http://example.s3.com/image2.png",
-                "오늘은 1000원 할인",
-                "yet",
-                LocalDateTime.of(2024, 2, 24, 21, 0, 0),
-                List.of("오늘은 1000원 할인", "제철 고등어 드세요", "너만 오면 고!")
-        );
+                "yet"
+                );
 
         Posting posting3 = new Posting(
                 3L,
-                -1L,
-                -1L,
+                24L,
+                LocalDate.of(2024, 4, 2),
+                LocalTime.of(12, 30),
+                "제육볶음",
                 null,
+                "신메뉴 시식해보세요",
                 "와 정말 맛있다!",
-                "done",
-                LocalDateTime.of(2024, 3, 2, 9, 0, 0),
-                List.of("와 정말 맛있다!", "오늘 요리 맛있습니다!", "잘먹었습니다!")
-        );
+                List.of("와 정말 맛있다!", "오늘 요리 맛있습니다!", "잘먹었습니다!"),
+                "안녕하세요! 오늘은 제육볶음이 준비되어 있습니다. 많이 오세요!",
+                "http://example.s3.com/image3.png",
+                "done"
+                );
 
         // 더미 데이터 저장
         postingRepository.saveAll(List.of(posting1, posting2, posting3));
     }
+
 }
