38dc249c68150ca5f07788eb47c56b0987199d59
namnam99 <onep5286@gmail.com>
2024-03-23 16:02:25 +0900

    refactor : Posting.java 수정

--- src/main/java/com/erica/gamsung/posting/domain/Posting.java
+++ src/main/java/com/erica/gamsung/posting/domain/Posting.java
@@ -7,7 +7,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Entity
@@ -18,12 +20,21 @@ public class Posting {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long reservationId;
     private Long userId;
-    private String imageUrl;
-    private String fixedContent;
-    private String state; // yet, ready, done
-    private LocalDateTime reservedAt;
+
+    private LocalDate date;
+    private LocalTime time;
+
+    private String menu;
+    private String event;
+    private String message;
+
     @Convert(converter = StringListConverter.class)
     private List<String> contents;
+
+    private String fixedContent;
+
+    private String imageUrl;
+
+    private String state; // yet, ready, done
 }
