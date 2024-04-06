b24cd2e6b166d813fb91efb1ac281ac306edcb74
namnam99 <onep5286@gmail.com>
2024-04-06 21:22:41 +0900

    fix : 충돌 수정

--- src/main/java/com/erica/gamsung/posting/utils/StringListConverter.java
+++ src/main/java/com/erica/gamsung/posting/utils/StringListConverter.java
@@ -4,6 +4,7 @@ import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Converter
@@ -20,11 +21,19 @@ public class StringListConverter implements AttributeConverter<List<String>, Str
      */
     @Override
     public String convertToDatabaseColumn(List<String> stringList) {
+        if (stringList == null) {
+            return null;
+        }
+
         return String.join(SPLIT_CHAR, stringList);
     }
 
     @Override
     public List<String> convertToEntityAttribute(String string) {
+        if (string == null || string.isEmpty()) {
+            return Collections.emptyList();
+        }
+
         return Arrays.asList(string.split(SPLIT_CHAR));
     }
 }
