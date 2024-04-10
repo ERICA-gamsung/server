b23083b065f82d8e98c8dc12bb456fef40d2a262
namnam99 <onep5286@gmail.com>
2024-04-10 19:46:11 +0900

    fix : date, time 포맷

--- src/main/java/com/erica/gamsung/posting/domain/Posting.java
+++ src/main/java/com/erica/gamsung/posting/domain/Posting.java
@@ -1,8 +1,10 @@
 package com.erica.gamsung.posting.domain;
 import com.erica.gamsung.posting.utils.ImageUrListConverter;
 import com.erica.gamsung.posting.utils.StringListConverter;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -19,7 +21,11 @@ public class Posting {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long reservationId;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+    @DateTimeFormat(pattern = "HH:mm")
     private LocalTime time;
 
     private String menu;
