217d415bfbd1f40cef970ecd545ec7078e6defdf
오유정 <ohyujeong@oyujeong-ui-MacBookPro.local>
2024-04-18 23:07:27 +0900

    properties -> yml 변경, config 폴더 생성

--- src/main/java/com/erica/gamsung/global/config/SwaggerConfig.java
+++ src/main/java/com/erica/gamsung/global/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.erica.gamsung;
+package com.erica.gamsung.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
--- src/main/java/com/erica/gamsung/image/service/ImageService.java
+++ src/main/java/com/erica/gamsung/image/service/ImageService.java
@@ -8,12 +8,15 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.erica.gamsung.posting.domain.Posting;
 import com.erica.gamsung.posting.repository.PostingRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
+import software.amazon.awssdk.services.s3.S3Client;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,7 +27,7 @@ import java.util.Objects;
 @Service
 public class ImageService {
     private final PostingRepository postingRepository;
-
+    private String bucketName = "gamsung-bucket";
     public List<String> uploadImage(PostImageRequest postImageRequest,Long reservationId){
         Posting post = postingRepository.findById(reservationId).get();
         int count = 1;
@@ -42,7 +45,6 @@ public class ImageService {
                     throw new ResponseStatusException(HttpStatus.FORBIDDEN,"jpg,png 파일만 가능합니다.");
             }
             final AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard().withRegion(Regions.AP_NORTHEAST_2).build();
-            String bucketName = "gamsung-bucket";
             try {
                 String fileName = "";
                 fileName = reservationId+"_"+count+type;
