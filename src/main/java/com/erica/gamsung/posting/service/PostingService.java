d9181cc8b68ecfd7a963b4222fd6ec496a3f9344
namnam99 <onep5286@gmail.com>
2024-05-02 19:54:25 +0900

    fix : ApplicationEventPublisher 삭제

--- src/main/java/com/erica/gamsung/posting/service/PostingService.java
+++ src/main/java/com/erica/gamsung/posting/service/PostingService.java
@@ -5,7 +5,6 @@ import com.erica.gamsung.posting.domain.Posting;
 import com.erica.gamsung.posting.repository.PostingRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
