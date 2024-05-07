4edcf6be526cf6b5a5914b2538158d57d03e09c9
오유정 <yujeong0720@gmail.com>
2024-05-07 12:11:35 +0900

    redis 연습

---
+++ src/main/java/com/erica/gamsung/global/config/redis/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.erica.gamsung.global.config.redis;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    Optional<RefreshToken> findByAuthId(String authId);
+}
