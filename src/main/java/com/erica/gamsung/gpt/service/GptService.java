cd7a74b68631d1ab95ac8f877c3bf500834836c9
namnam99 <onep5286@gmail.com>
2024-05-24 20:56:54 +0900

    fix : prompt 수정

--- src/main/java/com/erica/gamsung/gpt/service/GptService.java
+++ src/main/java/com/erica/gamsung/gpt/service/GptService.java
@@ -12,6 +12,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -32,18 +33,36 @@ public class GptService {
         String event = posting.getEvent();
         String message = posting.getMessage();
 
+        int cnt = 2;
+
         String prompt = String.format("""
-                    나는 음식점을 운영하고 있어. 내 음식점을 홍보하기 위한 홍보 문구를 작성해. 홍보 문구는 홍보할 메뉴, 이벤트, 고객에게 전달하고 싶은 메시지가 있으면 고려해서 3가지  작성해. 각 버전 사이에는 "@" 기호를 구분자로 사용하고 줄 바꿈 문자는 사용하면 안돼.
+                    내가 운영하는 음식점을 소개하고 싶어. 내 음식점을 홍보하기 위한 홍보 글를 작성해줘.
+                    글 작성 시 고려해야 할 사항 :
+                    1. 각 홍보 글은 최소 5문장 이상으로 작성해.
                     """);
         if (menu != null) {
-            prompt += String.format("홍보할 메뉴 : %s\n", menu);
+            prompt += String.format("%d. 홍보하고싶은 메뉴는 %s(이)야. %s 맛집으로 홍보를 해.\n", cnt, menu, menu);
         }
+        else {
+            prompt += String.format("%d. 홍보하고싶은 메뉴는 없으니 간단한 인사나 근황 소개와 함께 한 번 방문해보는 것은 어떤지 작성해.\n", cnt);
+        }
+
         if (event != null) {
-            prompt += String.format("이벤트 : %s\n", event);
+            prompt += String.format("%d. %s 이벤트를 한다는 것을 포함해서 작성하고, 고객들이 이 글을 보고 이벤트를 참여할 수 있게 홍보를 해.\n", ++cnt, event);
         }
+
         if (message != null) {
-            prompt += String.format("고객에게 전달하고 싶은 메시지 : %s\n", message);
+            prompt += String.format("%d. 고객들에게 \"%s\"라는 말을 전하고 싶어. 작성할 글의 스타일에 맞게 수정해서 추가해.\n", ++cnt, message);
         }
+
+        prompt += String.format("""
+                위 %d개의 사항(들)을 모두 고려한 서로 다른 3개의 홍보 글을 작성하고 각 버전 사이에 "@"기호를 1개 사용해서 출력해.
+                예를 들어 작성한 3개의 버전이 버전1 : "안녕", 버전2 : "안녕하세요.", 버전3 : "안녕하십니까!"라면, 아래의 출력 예시와 같이 출력해.
+                출력 예시 :
+                안녕@
+                안녕하세요.@
+                안녕하십니까!
+                """, cnt);
 
         return prompt;
     }
